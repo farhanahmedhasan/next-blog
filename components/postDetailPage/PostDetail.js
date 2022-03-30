44191dbb34fd37f905182dc01ecb20cd3284f065
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-03-30 22:06:14 +0600

    Added Build

--- components/postDetailPage/PostDetail.js
+++ components/postDetailPage/PostDetail.js
@@ -1,13 +1,17 @@
 import Image from "next/image";
 
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
 
-import PostContent from "../posts/PostContent";
 import PostHeader from "../posts/PostHeader";
 
 import styles from "./postDetail.module.css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 const PostDetail = ({ post }) => {
   const { title, image, content, slug } = post;
